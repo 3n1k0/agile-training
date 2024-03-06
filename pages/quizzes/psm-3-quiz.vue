21146e0d00b2a2e78c150fde6f5519025c952444
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2024-03-06 10:47:16 +0100

    initial commit

---
+++ pages/quizzes/psm-3-quiz.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>PSM III</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ratione,
+      molestiae dolores magnam illo, dicta eum unde at nisi similique et
+      laudantium. Corporis atque laboriosam ab quasi debitis, saepe eos!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
