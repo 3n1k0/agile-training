27f98a8b6b7180f648a93e9176ef75f399f734c7
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2024-03-14 09:54:13 +0100

    style navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,19 +1,40 @@
 <template>
-  <nav class="container mx-auto p-4 flex justify-between items-center">
-    <div class="logo font-bold text-xl">
-      <NuxtLink to="/">Agile Mindset Mastery</NuxtLink>
-    </div>
-    <ul class="flex gap-4">
-      <li>
-        <NuxtLink :class="{ active: $route.path === '/' }" to="/"
-          >Home</NuxtLink
-        >
-      </li>
-      <li>
+  <nav class="bg-white shadow">
+    <div class="container mx-auto px-6 py-3 flex justify-between items-center">
+      <NuxtLink to="/" class="text-xl font-semibold text-gray-700"
+        >Agile Mindset Mastery</NuxtLink
+      >
+      <div class="flex-1 ml-10">
+        <div class="relative max-w-sm">
+          <input
+            type="text"
+            class="w-full bg-gray-100 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-0 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            placeholder="Search"
+          />
+          <div
+            class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="flex items-center space-x-1">
         <NuxtLink
-          to="/what-is-agile"
-          >What is Agile?</NuxtLink
           class="py-2 px-3 text-gray-700 hover:text-gray-900 hidden md:block"
+          :class="{ active: $route.path === '/' }"
           to="/"
           >Home</NuxtLink
         >
