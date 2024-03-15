d49915c79ce30dd036e28afbcd113c19424e50b2
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2024-03-15 13:50:45 +0100

    implement store

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="blog-list">
-    <div v-if="isLoading" class="flex flex-col justify-center">
+    <div v-if="blogpostStore.isLoading" class="flex flex-col justify-center">
       <p class="text-center w-full">Loading...</p>
       <img
         class="loader"
@@ -9,32 +9,25 @@
       />
     </div>
     <div class="blog-cards" v-else>
-      <BlogCard v-for="post in blogPosts" :key="post.id" :post="post" />
+      <BlogCard
+        v-for="post in blogpostStore.blogPosts"
+        :key="post.id"
+        :post="post"
+      />
     </div>
   </div>
 </template>
 
 <script setup>
-import {
-  blogPosts,
-  isLoading,
-  fetchBlogPosts,
-} from "~/services/contentfulService";
-import { onMounted } from "vue";
 import { useBlogpostStore } from "~/stores/blogpostStore";
 
 const blogpostStore = useBlogpostStore();
 
 onMounted(async () => {
   try {
-    isLoading.value = true;
-    blogPosts.value = await fetchBlogPosts();
     await blogpostStore.fetchBlogPosts();
-    console.log(blogpostStore);
   } catch (error) {
     console.error("Error fetching blog posts:", error);
-  } finally {
-    isLoading.value = false;
   }
 });
 </script>
@@ -65,41 +58,3 @@ onMounted(async () => {
   }
 }
 </style>
-
-<!-- <template>
-  <div class="blog-list">
-    <div v-if="blogpostStore.isLoading" class="flex flex-col justify-center">
-      <p class="text-center w-full">Loading...</p>
-      <img
-        class="loader"
-        src="/img/loader.gif"
-        sizes="200px sm:50px md:100px"
-      />
-    </div>
-    <div class="blog-cards" v-else>
-      <BlogCard
-        v-for="post in blogpostStore.blogPosts"
-        :key="post.id"
-        :post="post"
-      />
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { useBlogpostStore } from "~/stores/blogpostStore";
-import { onMounted } from "vue";
-
-const blogpostStore = useBlogpostStore();
-onMounted(async () => {
-  try {
-    await blogpostStore.fetchBlogPosts();
-  } catch (error) {
-    console.error("Error fetching blog posts:", error);
-  }
-});
-</script>
-
-<style scoped>
-/* Styles */
-</style> -->
